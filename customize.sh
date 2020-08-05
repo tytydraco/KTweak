3b94110567cb5749d5de6e325ffa380b570ec403
Tyler Nijmeh <tylernij@gmail.com>
2020-08-05 12:33:26 -0700

    Fix typo in permission set
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 # Written by Draco (tytydraco @ GitHub)
 
 ui_print "[*] Setting executable permissions..."
-set_perm_recursive "$MODPATH/systen/bin" root root 0777 0755
+set_perm_recursive "$MODPATH/system/bin" root root 0777 0755
 
 # Do install-time script execution
 sh "$MODPATH/system/bin/ktweak"

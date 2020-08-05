d65b2ab4f981449b9a249de31cab7cb352e740ab
Tyler Nijmeh <tylernij@gmail.com>
2020-08-05 11:23:18 -0700

    Initialize as a system script
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- customize.sh
+++ customize.sh
@@ -1,6 +1,9 @@
 #!/system/bin/sh
 # Written by Draco (tytydraco @ GitHub)
 
+ui_print "[*] Setting executable permissions..."
+set_perm_recursive "$MODPATH/systen/bin" root root 0777 0755
+
 # Do install-time script execution
-sh "$MODPATH/service.sh"
+sh "$MODPATH/system/bin/ktweak"
 echo "[*] Executed service script during live boot. Reboot is not needed."

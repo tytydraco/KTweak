f328be209a2d7e40508b536b15e6629db60886c2
Tyler Nijmeh <tylernij@gmail.com>
2020-08-05 18:50:51 -0700

    Sleep until boot completes properly
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- service.sh
+++ service.sh
@@ -1,13 +1,5 @@
 #!/system/bin/sh
 # Written by Draco (tytydraco @ GitHub)
 
-# Wait 60s into boot before applying changes
-echo "[*] Waiting for 60s of uptime."
-while [[ `cat /proc/uptime | awk '{print $1}' | awk -F. '{print $1}'` -lt 60 ]]
-do
-	sleep 1
-done
-echo "[*] Done waiting."
-
 # Setup tweaks
 ktweak

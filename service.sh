e902612dc789b8ba773a978c0a4af872821ee691
Tyler Nijmeh <tylernij@gmail.com>
2020-08-20 08:52:21 -0700

    Fix running paralell with init
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- service.sh
+++ service.sh
@@ -1,5 +1,15 @@
 #!/system/bin/sh
 # Written by Draco (tytydraco @ GitHub)
 
+# Wait for boot to finish completely
+dbg "Sleeping until boot completes."
+while [[ `getprop sys.boot_completed` -ne 1 ]]
+do
+       sleep 1
+done
+
+# Sleep an additional 20s to ensure init is finished
+sleep 20
+
 # Setup tweaks
 ktweak

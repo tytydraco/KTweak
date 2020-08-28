d569eb92973186a7230730115a444f347870974c
Tyler Nijmeh <tylernij@gmail.com>
2020-08-28 10:02:41 -0700

    Wait for internal storage to mount
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- service.sh
+++ service.sh
@@ -2,13 +2,13 @@
 # Written by Draco (tytydraco @ GitHub)
 
 # Wait for boot to finish completely
-while [[ `getprop sys.boot_completed` -ne 1 ]]
+while [[ `getprop sys.boot_completed` -ne 1 ]] && [[ ! -d "/sdcard" ]]
 do
        sleep 1
 done
 
-# Sleep an additional 90s to ensure init is finished
-sleep 90
+# Sleep an additional 10s to ensure init is finished
+sleep 10
 
 # Setup tweaks
 ktweak

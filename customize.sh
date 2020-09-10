129b9dfb0a06d15624b977a522232c0898f4ee36
Tyler Nijmeh <tylernij@gmail.com>
2020-09-09 17:43:48 -0700

    Switch master branch to raw script
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- customize.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-# Written by Draco (tytydraco @ GitHub)
-
-ui_print "[*] Setting executable permissions..."
-set_perm_recursive "$MODPATH/system/bin" root root 0777 0755
-
-# Do install-time script execution
-ui_print "[*] Executing during live boot..."
-sh "$MODPATH/system/bin/ktweak"

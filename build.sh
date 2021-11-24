bfd4e469765ead85c86322bd814cc90ef63b196b
Tyler Nijmeh <tylernij@gmail.com>
2021-11-24 15:45:43 -0800

    Finish build script
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-TS="$(date +%F_%M-%H-%S)"
+TIMESTAMP="$(date +%F_%M-%H-%S)"
+HASH="$(git rev-parse HEAD)"
+ZIP="KTweak-MM_${TIMESTAMP}_${HASH}.zip"
 
+rm -f KTweak-MM_*.zip
+zip -0 -r -ll "$ZIP" META-INF/ customize.sh module.prop service.sh

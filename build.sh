1a9c6f4a0ce88c1236710f6ce11439f46774e162
Tyler Nijmeh <tylernij@gmail.com>
2021-11-25 01:30:06 -0800

    Build selected branches
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-TIMESTAMP="$(date +%F_%M-%H-%S)"
-HASH="$(git rev-parse HEAD)"
-ZIP="KTweak-MM_${TIMESTAMP}_${HASH}.zip"
+BRANCHES=(balance budget latency throughput)
 
-rm -f KTweak-MM_*.zip
-zip -0 -r -ll "$ZIP" META-INF/ customize.sh module.prop service.sh
+rm -rf KTweak-MM*.zip
+
+for branch in ${BRANCHES[@]}
+do
+	echo " * Building $branch..."
+	TIMESTAMP="$(date +%F_%M-%H-%S)"
+	HASH="$(git rev-parse HEAD)"
+	ZIP="KTweak-MM-${branch}_${TIMESTAMP}_${HASH}.zip"
+
+	cp customize.sh customize.sh.bk
+	sed -i "s/BRANCH=\"balance\"/BRANCH=\"$branch\"/" customize.sh
+	zip -0 -r -ll "$ZIP" META-INF/ customize.sh module.prop service.sh
+	mv customize.sh.bk customize.sh
+
+	echo
+done
